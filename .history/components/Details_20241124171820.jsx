5b222e16cf892adb1f9b96f0fd9c264a560918b2
Samantha Yan <yyan30@my.bcit.ca>
2024-11-24 17:21:10 -0800

    updated

--- .history/components/Details_20241124171820.jsx
+++ .history/components/Details_20241124171820.jsx
@@ -4,7 +4,7 @@ import '../src/App.css'
 export default function Details({ country }) {
   return (
     <div className='cards-container'>
-      <h1>{country.name.official}</h1>
+      <h1 className='name'>{country.name.official}</h1>
       <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
       <div className='country-info'>
         <p>
